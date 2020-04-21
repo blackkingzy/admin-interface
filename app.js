86d78acc2c1fb95b762f2dee7fd3da0fa7d7d6a6
blackkingzy <1003975792@qq.com>
2020-04-21 23:16:06 +0800

    update

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 // var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const jwt = require('jsonwebtoken');
-
-
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const jwt = require("jsonwebtoken");
 
 var app = express();
+app.use(express.static(path.join(__dirname, "public"))); //设置静态文件夹
 
-
-app.use(logger('dev')); //记录日志的中间件
+app.use(logger("dev")); //记录日志的中间件
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));// option属性可以设置各种条件
+app.use(express.static(path.join(__dirname, "public"))); // option属性可以设置各种条件
 
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Credentials", true)
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-    res.header("X-Powered-By", ' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8")
-    next()
-})
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
-
-require('./api')(app)
+require("./api")(app);
 app.listen(3001, () => {
-    console.log('Server is running at http://localhost:3001')
-})
-
+  console.log("Server is running at http://localhost:3001");
+});
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
--- controllers/global/index.js
+++ controllers/global/index.js
@@ -1,18 +1,17 @@
-const whiteList = ['/user/login', '/user/logout']
-const token = require('../../utils/token')
+const whiteList = ["/user/login", "/user/logout"];
+const token = require("../../utils/token");
 exports.userTokenVerify = function (req, res, next) {
-    if (whiteList.indexOf(req.originalUrl) !== -1) {
-        next()
+  if (whiteList.indexOf(req.originalUrl) !== -1) {
+    next();
+  } else {
+    const userToken = req.headers.authorization;
+    const result = token.verify(userToken);
+    if (result) {
+      // console.log('解析的数据', data)
+      next();
     } else {
-        const userToken = req.headers.authorization
-        const result = token.verify(userToken)
-        if (result) {
-            // console.log('解析的数据', data)
-            next()
-        }
-        else {
-            const params = { code: 50008 }
-            res.send(params)
-        }
+      const params = { code: 50008, message: "Token expired" };
+      res.send(params);
     }
-}
+  }
+};
